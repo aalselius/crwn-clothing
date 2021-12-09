2696e1cf32e41ed3e70211ed42ceb9557d78099d
AAlselius <alselius@telia.com>
2021-12-09 18:52:43 +0100

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { connect, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selectors";
+
+import './collection.styles.scss';
+
+const CollectionPage = () => {
+    let params = useParams();
+    const collection = useSelector(selectCollection(params.collectionId));
+    const { title, items } = collection;
+    return (
+    <div className='collection-page'>
+        <h2 className='title'>{ title }</h2>
+        <div className='items'>
+        {
+            items.map(item => <CollectionItem key={item.id} item={item} />)
+        }
+        </div>
+    </div>
+)};
+
+const mapStateToProps = (state, ownProps) => ({
+  // collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
